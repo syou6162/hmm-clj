558773086cdfa04ad423f691e5917a8f87b7d99f
Yasuhisa Yoshida <syou6162@gmail.com>
2012-11-03 23:43:02 +0900

    小さな関数へのテストを追加。もう一つhmmのforwardとviterbiへのテストを追加
    - 前のテストはviterbiの答が間違っていた

--- test/hmm_clj/hmm_test.clj
+++ test/hmm_clj/hmm_test.clj
@@ -2,6 +2,16 @@
   (:use clojure.test
         hmm-clj.hmm))
 
+(deftest indexed-test
+  (are [x y] (= x y)
+       (indexed []) '()
+       (indexed [:a :b :c]) '([0 :a] [1 :b] [2 :c])))
+
+(deftest argmax-test
+  (are [x y] (= x y)
+       (argmax [1 2 3 9]) [3 9]
+       (argmax [-1 -3 -2 3]) [3 3]))
+
 (deftest scaling-test
   (are [x y] (= x y)
 
@@ -17,7 +27,7 @@
 ;; 前向きアルゴリズム、Vitebiアルゴリズム
 ;; http://d.hatena.ne.jp/syou6162/20100708/1278577199
 
-(let [my-hmm (struct
+(def my-hmm1 (struct
               hmm
               5
               5
@@ -31,9 +41,30 @@
                            (double-array [0.7, 0.0, 0.0, 0.3, 0.0])
                            (double-array [0.3, 0.2, 0.1, 0.2, 0.2])
                            (double-array [0.5, 0.1, 0.0, 0.4, 0.0])
-                           (double-array [0.6, 0.3, 0.0, 0.1, 0.0])]))]
-  (deftest forward-test
-    (is (= (forward my-hmm [0 1 2 3 4])) 8.542799999999998E-4))
-  (deftest viterbi-test
-    (is (= (viterbi my-hmm [0 1 2 3 4])
-           ['(0 2 0 1 0) 3.628800000000001E-4]))))
+                           (double-array [0.6, 0.3, 0.0, 0.1, 0.0])])))
+
+(deftest forward-test1
+  (is (= (forward my-hmm1 [0 1 2 3 4])) 8.542799999999998E-4))
+
+(deftest viterbi-test1
+  (is (= (viterbi my-hmm1 [0 1 2 3 4])
+         ['(0 2 4 1 0) 3.628800000000001E-4])))
+
+;; http://ja.wikipedia.org/wiki/ビタビアルゴリズム
+
+(def my-hmm2 (struct
+              hmm
+              2 3
+              (double-array [0.6, 0.4])
+              (into-array [(double-array [0.1 0.4 0.5])
+                           (double-array [0.6 0.3 0.1])])
+              (into-array [(double-array [0.7 0.3])
+                           (double-array [0.4 0.6])])))
+
+(deftest forward-test2
+  (is (= (forward my-hmm2 [0 1 2])
+         0.03361199999999998)))
+
+(deftest viterbi-test2
+  (is (= (viterbi my-hmm2 [0 1 2])
+         ['(1 0 0) 0.013439999999999999])))
