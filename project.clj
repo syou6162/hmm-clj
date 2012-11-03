99fe514a3199736fffac05645ba446e9cd6ac8cf
Yasuhisa Yoshida <syou6162@gmail.com>
2012-11-03 23:20:54 +0900

    added the utility library and specify the encoding type

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "https://github.com/syou6162/hmm-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [info.yasuhisay/clj-utils "0.1.0-SNAPSHOT"]]
+  :jvm-opts ["-Xmx8g" "-server" "-Dfile.encoding=UTF-8"]
+  :main hmm-clj.core)
